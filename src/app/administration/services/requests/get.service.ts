72ffd522bd3f6ab4f855b06cd317701222c6f902
zybrik0007 <zybrik0007@rambler.ru>
2020-03-23 16:05:58 +0300

    Добавление файлов

--- src/app/administration/services/requests/get.service.ts
+++ src/app/administration/services/requests/get.service.ts
@@ -1,12 +1,9 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-
+import {Router} from '@angular/router';
 
 @Injectable({providedIn: 'root'})
 export class GetService {
   constructor(
     private http: HttpClient) {}
-
-    getCategory()
-
 }
